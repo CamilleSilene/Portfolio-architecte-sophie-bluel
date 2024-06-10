18574958242b88a311df583ba50c9614db0a9911
CamilleSilene <trillatcam@gmail.com>
2024-06-10 21:15:13 +0200

    bandeau + btn modifier+ cursor pointer

--- FrontEnd/auth.js
+++ FrontEnd/auth.js
@@ -1,8 +1,13 @@
+//fonction pour checker la présence du token et maintenir la connexion de l'utilisateur
+
+
+//récupération de l'id nav-connexion et ajout d'un événement au click
+//si click sur logout : le token est supprimé / si plus de token, on retourne sur la page login
 document
   .getElementById("nav-connexion")
   .addEventListener("click", function (event) {
     let navConnection = event.target;
-    if (navConnection.innerText == "Logout") {
+    if (navConnection.innerText == "logout") {
       window.localStorage.removeItem("token");
       checkToken();
     } else {
@@ -10,14 +15,19 @@ document
     }
   });
 
+
+  //si le token est stocké dans le local, on affiche logout
+  //sinon, on affiche login
+  //affichage du bandeau édition et du bouton modifier
 function checkToken() {
   let loggedIn = window.localStorage.getItem("token") != null;
-  document.getElementById("nav-connexion").innerText = loggedIn ? "Logout" : "Login";
+  document.getElementById("nav-connexion").innerText = loggedIn ? "logout" : "login";
   showElementById("bandeau-id", loggedIn);
+  showElementById("btn-modifier", loggedIn);
 }
 
 function showElementById(elementId, isShown) {
   if (document.getElementById(elementId) !== null) {
-    document.getElementById(elementId).style.display = isShown ? "block" : "none";
+    document.getElementById(elementId).style.display = isShown ? "flex" : "none";
   }
 }
